c8839d71c6675c90680e29b6d5dd39fd4ee979e9
Jared Mountan <jmountan@copado.com>
2024-04-09 14:45:01 -0400

    Added a function to send emails to many

--- libraries/smtp.py
+++ libraries/smtp.py
@@ -50,13 +50,34 @@ def send_email_Attachments(subject, body, sender, recipients, user, password, fi
     logger.warn("mail send")
     smtp_server.quit()
 
-def send_email_many(subject, body, sender, recipients, user, password):
+def send_email_to_many(subject, body, sender, recipients, user, password):
     msg = MIMEMultipart()
     msg['Subject'] = subject
     msg['From'] = sender
     msg['To'] = ", ".join(recipients)
     msg.attach(MIMEText(body))
 
+    smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    smtp_server.login(user, password)
+    smtp_server.sendmail(sender, recipients, msg.as_string())
+    logger.warn("mail send")
+    smtp_server.quit()
+
+def send_email_Attachments_to_many(subject, body, sender, recipients, user, password, files):
+    msg = MIMEMultipart()
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = ", ".join(recipients)
+    msg.attach(MIMEText(body))
+ 
+    for path in files:
+        part = MIMEBase('application', "octet-stream")
+        with open(path, 'rb') as file:
+            part.set_payload(file.read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition',
+                        'attachment; filename={}'.format(Path(path).name))
+        msg.attach(part)
     smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     smtp_server.login(user, password)
     smtp_server.sendmail(sender, recipients, msg.as_string())
